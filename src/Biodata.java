442e4b30195fcd5a546c499b02e0c0cf8cb1d4bc
FemiDeji <adeniyioluwafemiayodeji@gmail.com>
2023-02-02 16:11:03 -0800

    Initial commit

---
+++ src/Biodata.java
@@ -0,0 +1,19 @@
+public class Biodata {
+     public static void main(String[] args){
+         System.out.println("BIODATA");
+         System.out.println("Name: Adeniyi Oluwafemi Ayodeji");
+         System.out.println("Date of Birth:16/08/2003");
+         System.out.println("Gender: Male");
+         System.out.println("Nationality: Nigerian");
+         System.out.println("State of Origin: Ondo");
+         System.out.println("Matric No: 22/1119");
+         System.out.println("Department: Computer Science");
+         System.out.println("Course: Computer Science");
+         System.out.println("Level: 200L");
+         System.out.println("Course Code:CSC 201");
+         System.out.println("Email Address: adeniyioluwafemiayodeji@gmail.com");
+         System.out.println("Phone Number: 08087830966");
+         System.out.println("Address: 4,Adeniran close,Ipaja,Lagos state");
+
+     }
+}
